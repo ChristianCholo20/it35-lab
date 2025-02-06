ce7955bab0f688ccc4cd1e6451ae213092b380a1
ChristianCholo20 <chologodlike55@gmail.com>
2025-02-06 08:58:48 +0800

    Updates

--- assets/keyboard2-legacy-CK9xLQ6y.js
+++ assets/keyboard2-legacy-CK9xLQ6y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DXB_qrU2.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-owDTE5ez.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
