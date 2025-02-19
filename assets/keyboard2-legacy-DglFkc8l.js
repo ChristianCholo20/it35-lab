1fd72d7b1e85762e9c2f5596f0d3d42d8f929dd2
JABENIAO <jboyjabeniao#gmail.com>
2025-02-20 07:58:39 +0800

    Updates

--- assets/keyboard2-legacy-DglFkc8l.js
+++ assets/keyboard2-legacy-DglFkc8l.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DHtQOHGn.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Db7VhlXU.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BCLjUqHj.js
+++ assets/swipe-back-legacy-BCLjUqHj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DHtQOHGn.js","./index3-legacy-CiKI6NyZ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Db7VhlXU.js","./index3-legacy-Wm6QGnwR.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
