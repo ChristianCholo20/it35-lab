ba0dd98d8eea05ce4dc8e17cfc1829fca181c0da
ChristianCholo20 <chologodlike55@gmail.com>
2025-04-30 07:45:33 +0800

    Updates

--- assets/ios.transition-legacy-DDcGMqtQ.js
+++ assets/ios.transition-legacy-DDcGMqtQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-i4MgjEAl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CejsPNfy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
