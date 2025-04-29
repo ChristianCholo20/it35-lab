b05f610e3400f6f970fb2c57fc6fb2467b1fd4b7
ChristianCholo20 <chologodlike55@gmail.com>
2025-04-30 07:32:18 +0800

    Updates

--- assets/md.transition-legacy-3zhTwd86.js
+++ assets/md.transition-legacy-3zhTwd86.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DntUOwzK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-i4MgjEAl.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
